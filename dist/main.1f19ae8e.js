c3decb0dc819da97c35c22acb1be5fcfaa6a17d0
xiaoshu <1324214367@qq.com>
2020-11-21 11:28:05 +0800

    branch

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -11610,7 +11610,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55717" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56064" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
